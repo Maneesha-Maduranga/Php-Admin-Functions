828dee1ad9ee98a7473ce3b6f80cd42d79e537dc
Maneesha Maduranga <101623802+Maneesha-Maduranga@users.noreply.github.com>
2022-12-16 22:57:06 +0530

    Delete Frontend directory
    Delete Front End part

--- Frontend/Temp/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer footer-center p-4 bg-base-300 text-base-content">
-  <div>
-    <p>Copyright © 2022 - All right reserved by GalaxyStore Industries Ltd</p>
-  </div>
-</footer>
-</div>
-</body>
-</html>
