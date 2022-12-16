1823d883c8abb4998d4cb27258604af8cd218bdf
Maneesha Maduranga <101623802+Maneesha-Maduranga@users.noreply.github.com>
2022-12-16 10:06:29 +0530

    Add files via upload

---
+++ Temp/Footer.php
@@ -0,0 +1,12 @@
+
+
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+  <div>
+    <p>Copyright © 2022 - All right reserved by GalaxyStore Industries Ltd</p>
+  </div>
+</footer>
+</div>
+</body>
+</html>
+
+
---
+++ Temp/Header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GalaxyStore | Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.43.2/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+
+<body>
+<div class="flex flex-col justify-between h-screen">
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a href="../Backend/index.php"><img src="../Backend/img/store.png" alt="" class="w-8"></a>
+    <h1 class="text-2xl px-2">Galaxy Store</h1>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="../Backend/additem.php">ADD Item</a></li>
+    </ul>
+  </div>
+</div>
