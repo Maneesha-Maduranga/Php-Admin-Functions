13ff7cb9c9b0cad60e8a8683a08aaa9141a7c555
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2022-12-30 13:21:03 +0530

    Change links

--- Temp/Header.php
+++ Temp/Header.php
@@ -14,7 +14,7 @@
 <div class="flex flex-col justify-between h-screen">
 <div class="navbar bg-base-100">
   <div class="flex-1">
-    <a href="../Backend/index.php"><img src="../Backend/img/store.png" alt="" class="w-8"></a>
+    <a href="/GalaxyStore/index.php"><img src="../Backend/img/store.png" alt="" class="w-8"></a>
     <h1 class="text-2xl px-2">Galaxy Store</h1>
   </div>
   <div class="flex-none">
