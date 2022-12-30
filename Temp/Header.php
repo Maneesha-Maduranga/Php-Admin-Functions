441cf05857f2135ef0b7ad4dcac386bf0aa5d182
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2022-12-30 13:41:41 +0530

    change ui

--- Temp/Header.php
+++ Temp/Header.php
@@ -14,8 +14,8 @@
 <div class="flex flex-col justify-between h-screen">
 <div class="navbar bg-base-100">
   <div class="flex-1">
-    <a href="/GalaxyStore/index.php"><img src="../Backend/img/store.png" alt="" class="w-8">
-    <h1 class="text-2xl px-2 border border-black">Galaxy Store</h1>
+    <a href="/GalaxyStore/index.php"><img src="https://cdn-icons-png.flaticon.com/512/2991/2991473.png" alt="" class="w-8">
+    <h1 class="text-2xl px-2">Galaxy Store</h1>
     </a>
   </div>
   <div class="flex-none">
